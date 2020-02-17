6cc056563d41a66bc33d17f8485fbe2e1877b034
Jared Hood <jwh6ry@virginia.edu>
2020-02-17 12:32:26 -0500

    updated .gitignore

--- myenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/emilynguyen/Desktop/CS 3240/project-103-red-robins/myenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
